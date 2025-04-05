edceae3a37d3394fa0e6250ef056dcc82c612da1
abhayraj2108 <abhayraj2108@github.com>
2025-04-05 11:55:48 +0530

    ignored files

--- backend/mailRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { sendMail } = require('./mailController.js');
-
-const router = express.Router();
-
-router.post('/send-email', sendMail);
-
-module.exports = router;
